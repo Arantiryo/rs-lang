934609e4ee9d00829735622bea7b5abe04accc94
Ivan Tsenilov <arantirex@gmail.com>
2022-02-07 21:45:28 +0300

    feat: implement games page

--- rs-lang/src/Routes/Routes.tsx
+++ rs-lang/src/Routes/Routes.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import Games from "../Containers/Games/Games";
 import Home from "../Containers/Home/Home";
 import Login from "../Containers/Login/Login";
 import Signup from "../Containers/Signup/Signup";
@@ -15,6 +16,9 @@ export default function Routes() {
       </Route>
       <Route exact path="/login">
         <Login />
+      </Route>
+      <Route exact path="/games">
+        <Games />
       </Route>
     </Switch>
   );
