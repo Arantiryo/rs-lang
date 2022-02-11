0983bb94915e00c03e1887e0ef5c7babf08c4f93
Ilya Ivanov <konhote@gmail.com>
2022-02-11 16:40:49 +0300

    fix: enlarge the areas of clicking on navs

--- rs-lang/src/components/Header/Navbar/Nav.tsx
+++ rs-lang/src/components/Header/Navbar/Nav.tsx
@@ -1,6 +1,6 @@
+import { MdExitToApp } from "react-icons/md";
 import { NavLink } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "../../../app/hooks";
-import { MdExitToApp } from "react-icons/md";
 import LoaderButton from "../../LoaderButton/LoaderButton";
 import { initialState, updateUserInfo } from "../../LoginForm/loginSlice";
 
@@ -18,39 +18,53 @@ const RightNav = ({ open }: NavProps) => {
 
   return (
     <ul
-      className={`list-none flex flex-nowrap z-50 mw:flex-col mw:bg-gray-700 
+      className={`list-none flex items-center flex-nowrap z-50 mw:flex-col mw:bg-gray-700 
       mw:fixed ${open ? "mw:translate-x-0" : "mw:translate-x-full"} 
-      mw:top-0 mw:right-0 mw:h-screen mw:w-[280px] mw:pt-14 mw:transition-transform mw:duration-500`}
+      mw:top-0 mw:right-0 mw:h-screen mw:w-[280px] mw:pt-14 mw:transition-transform mw:duration-500 mw:gap-3
+      
+      `}
     >
-      <li className="py-2 px-3 xl:px-8 text-white cursor-pointer text-center hover:text-yellow-500 transition-colors">
-        <NavLink to="/textbook" activeClassName="text-yellow-500">
+      <li>
+        <NavLink
+          className={"py-2 px-3 xl:px-8 text-white cursor-pointer text-center hover:text-yellow-500 transition-colors"}
+          to="/textbook" activeClassName="text-yellow-500">
           Учебник
         </NavLink>
       </li>
-      <li className="py-2 px-3 xl:px-8 text-white cursor-pointer text-center hover:text-yellow-500 transition-colors">
-        <NavLink to="/games" activeClassName="text-yellow-500">
+      <li>
+        <NavLink
+          className="py-2 px-3 xl:px-8 text-white cursor-pointer text-center hover:text-yellow-500 transition-colors"
+          to="/games" activeClassName="text-yellow-500">
           Игры
         </NavLink>
       </li>
-      <li className="py-2 px-3 xl:px-8 text-white cursor-pointer text-center hover:text-yellow-500 transition-colors">
-        <NavLink to="/team" activeClassName="text-yellow-500">
+      <li>
+        <NavLink
+          className="py-2 px-3 xl:px-8 text-white cursor-pointer text-center hover:text-yellow-500 transition-colors"
+          to="/team" activeClassName="text-yellow-500">
           О команде
         </NavLink>
       </li>
-      <li className="py-2 px-3 xl:px-8 text-white cursor-pointer text-center hover:text-yellow-500 transition-colors">
-        <NavLink to="/statistics" activeClassName="text-yellow-500">
+      <li>
+        <NavLink
+          className="py-2 px-3 xl:px-8 text-white cursor-pointer text-center hover:text-yellow-500 transition-colors"
+          to="/statistics" activeClassName="text-yellow-500">
           Статистика
         </NavLink>
       </li>
       {userInfo.userId === "" ? (
         <>
-          <li className="py-2 px-3 xl:px-8 text-white cursor-pointer text-center hover:text-yellow-500 transition-colors">
-            <NavLink to="/signup" activeClassName="text-yellow-500">
+          <li>
+            <NavLink
+              className="py-2 px-3 xl:px-8 text-white cursor-pointer text-center hover:text-yellow-500 transition-colors"
+              to="/signup" activeClassName="text-yellow-500">
               Регистрация
             </NavLink>
           </li>
-          <li className="text-white cursor-pointer text-center transition-colors bg-emerald-700 hover:bg-emerald-600">
-            <NavLink to="/login" activeClassName="text-yellow-500">
+          <li>
+            <NavLink
+              className="text-white cursor-pointer text-center transition-colors bg-emerald-700 hover:bg-emerald-600"
+              to="/login" activeClassName="text-yellow-500">
               <div className="py-2 px-6 xl:px-8 w-full mx-auto h-full ">
                 Войти
               </div>
