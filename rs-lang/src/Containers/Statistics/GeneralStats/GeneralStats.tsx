fce09d2e1fc77a69e804f2451cf9cbc8739ebcba
Ivan Tsenilov <arantirex@gmail.com>
2022-02-14 09:38:01 +0300

    feat: add general stats

--- rs-lang/src/Containers/Statistics/GeneralStats/GeneralStats.tsx
+++ rs-lang/src/Containers/Statistics/GeneralStats/GeneralStats.tsx
@@ -11,6 +11,24 @@ export default function GeneralStats() {
         src={pseudo1}
         alt="pseudo element"
       />
+      <div className="flex justify-end gap-[75px] w-[70%] pt-10">
+        <div className="flex flex-col items-center">
+          <span className="text-white font-extrabold text-[72px] leading-[84px]">
+            10
+          </span>
+          <span className="text-white text-[24px] leading-[28px]">
+            слов изучено
+          </span>
+        </div>
+        <div className="flex flex-col items-center">
+          <span className="text-white font-extrabold text-[72px] leading-[84px]">
+            66%
+          </span>
+          <span className="text-white text-[24px] leading-[28px]">
+            правильных ответов
+          </span>
+        </div>
+      </div>
     </div>
   );
 }
