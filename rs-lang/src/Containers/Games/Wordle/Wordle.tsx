afe9fad44067c577c56a18cd21b49f060882fbf2
Ivan Tsenilov <arantirex@gmail.com>
2022-02-19 10:39:55 +0300

    feat: implement selecting a random word for the game

--- rs-lang/src/Containers/Games/Wordle/Wordle.tsx
+++ rs-lang/src/Containers/Games/Wordle/Wordle.tsx
@@ -1,8 +1,41 @@
+import { useEffect, useState } from "react";
 import Header from "../../../components/Header/Header";
 import Main from "../../../components/Main/Main";
 import WordleGame from "../../../components/WordleGame/WordleGame";
+import IWord from "../../../interfaces/IWord";
+import { getWords } from "../../../utils/WebClients";
+
+const numberOfCategories = 6;
+const numberOfPages = 30;
+
+const getWord = async () => {
+  const categoryIndex = Math.floor(Math.random() * numberOfCategories);
+  const pageIndex = Math.floor(Math.random() * numberOfPages);
+  const fiveLetterWords: IWord[] = [];
+  let retries = 5;
+
+  while (fiveLetterWords.length === 0 && retries > 0) {
+    const words = await getWords(pageIndex, categoryIndex);
+    fiveLetterWords.push(...words.filter((w: IWord) => w.word.length === 5));
+    retries -= 1;
+  }
+
+  if (retries <= 0) {
+    throw Error("Could not get the word from the server");
+  }
+
+  return fiveLetterWords[Math.floor(Math.random() * fiveLetterWords.length)];
+};
 
 export default function Wordle() {
+  const [word, setWord] = useState("");
+
+  useEffect(() => {
+    getWord()
+      .then((res) => setWord(res.word))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div className="h-screen flex flex-col bg-wordle bg-bottom bg-cover bg-no-repeat">
       <div className="w-full">
@@ -10,7 +43,7 @@ export default function Wordle() {
       </div>
       <div className={`grow-[2]`}>
         <Main className="h-full" transparentBg={true}>
-          <WordleGame />
+          {!!word && <WordleGame word={word} />}
         </Main>
       </div>
     </div>
